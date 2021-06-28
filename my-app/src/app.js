b283bcef9121535c3e1940e59920375ca0cb7ea3
Damien Landouard <dlandouard@gmail.com>
2021-06-28 17:51:29 +0200

    mise à jour packages

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-<<<<<<< HEAD
-const mainRouter = require('./routes');
-=======
+
 const mainRouter = require('./routes/index');
->>>>>>> 2a133c9039c7912a919f25a07b0550211147997d
 
 app.use(
   cors({
