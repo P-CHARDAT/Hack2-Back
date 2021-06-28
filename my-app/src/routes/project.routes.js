d659228bb2390c1a186825ec84b8b3c4580dbcb6
Pierre <pierre.chardat@neuf.fr>
2021-06-28 19:17:21 +0200

    Ajout de deux routes infos dans le controllers et le fichier routes de project + mise à jour des termes en anglais et des mauvaise nominations de variables

--- my-app/src/routes/project.routes.js
+++ my-app/src/routes/project.routes.js
@@ -4,10 +4,14 @@ const {
   getProjects,
   createOneProject,
   updateOneProject,
+  getProjectInfos,
+  getProjectInfosById,
 } = require("../controllers/project.controllers");
 
 projectRoutes.get("/", getProjects);
 projectRoutes.get("/:id", getProjects);
+projectRoutes.get("/infos/", getProjectInfos);
+projectRoutes.get("/infos/:id", getProjectInfosById);
 projectRoutes.post("/", createOneProject, getProjects);
 projectRoutes.put("/:id", updateOneProject, getProjects);
 
